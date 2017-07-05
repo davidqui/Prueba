3400c301a9fd7d86718f1ce281553b27358c9ed9
Jorge A. Garcia E. <jgarcia@controltechcg.com>
2017-07-05 14:36:29 -0500

    Issue #115 (SICDI-Controltech) feature-115: Número de días del rango de búsqueda para bandejas

---
+++ doc-web/src/main/java/com/laamware/ejercito/doc/web/serv/BandejaService.java
@@ -0,0 +1,18 @@
+package com.laamware.ejercito.doc.web.serv;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Servicio para funciones asociadas a las bandejas.
+ * 
+ * @author jgarcia@controltechcg.com
+ * @since Jul 05, 2017
+ */
+/*
+ * 2017-07-05 jgarcia@controltechcg.com Issue #50 (SICDI-Controltech)
+ * feature-115.
+ */
+@Service
+public final class BandejaService {
+	
+}
