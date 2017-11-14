f81e0cbe7a1f5d910d324f07693dc8a05a65ee7a
Edison <edison.gonzalez@controltechcg.com>
2017-11-14 11:42:52 -0500

    issue #138 (SICDI-Controltech) feature-138: Se realiza el script donde se crea la nueva tabla, que permite relacionar el indicativo utilizado en el numero de radicado con el proceso.

---
+++ doc-web/sql/cambios/issue_feature_138.sql
@@ -0,0 +1,38 @@
+-- -----------------------------------------------------------------------------
+-- 2017-11-14 edison.gonzalez@controltechcg.com Feature #131 (SICDI-Controntech)
+-- feature-138
+-- Ajuste del contador del numero de radicado.
+-- -----------------------------------------------------------------------------
+
+-- -----------------------------------------------------------------------------
+-- 2017-11-14 edison.gonzalez@controltechcg.com Feature #138 (SICDI-Controntech)
+-- feature-138
+-- Creación de la tabla que relaciona el proceso con el indicativo del numero
+-- de radicado.
+-- -----------------------------------------------------------------------------
+
+CREATE TABLE "DOC"."PROCESO_RADICADO"(
+    "ID"            NUMBER(*,0) NOT NULL ENABLE, 
+    "PRO_ID"        NUMBER(*,0), 
+    "INDICATIVO"    NUMBER(1)   NOT NULL ENABLE, 
+    CONSTRAINT "PROCESO_RADICADO_PK" PRIMARY KEY ("ID"),
+    CONSTRAINT FK_PROCESO_RADICADO_1
+    FOREIGN KEY (PRO_ID)
+    REFERENCES PROCESO(PRO_ID)
+);
+
+-- INDEX
+
+CREATE INDEX PROCESO_RADICADO_IDX1 ON PROCESO_RADICADO(PRO_ID);
+
+-- SEQUENCE
+
+CREATE SEQUENCE  "DOC"."PROCESO_RADICADO_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 100;
+
+-- DATOS
+
+INSERT INTO "DOC"."PROCESO_RADICADO" (ID, PRO_ID, INDICATIVO) VALUES ('1', '9', '1');
+INSERT INTO "DOC"."PROCESO_RADICADO" (ID, PRO_ID, INDICATIVO) VALUES ('2', '8', '2');
+INSERT INTO "DOC"."PROCESO_RADICADO" (ID, PRO_ID, INDICATIVO) VALUES ('3', '41', '3');
+
+COMMIT;
