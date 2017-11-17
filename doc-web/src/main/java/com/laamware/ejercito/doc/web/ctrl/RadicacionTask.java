55d3e56e8f3d724d6110060f444e2990b26eec7d
Edison <edison.gonzalez@controltechcg.com>
2017-11-17 11:25:22 -0500

    feature #132 (SICDI-Controltech) feature-132: Se realiza la verificación de la ip y el nombre de la maquina donde se esta corriendo el proceso de reinicio de secuencias.

--- doc-web/src/main/java/com/laamware/ejercito/doc/web/ctrl/RadicacionTask.java
+++ doc-web/src/main/java/com/laamware/ejercito/doc/web/ctrl/RadicacionTask.java
@@ -7,6 +7,8 @@ import com.laamware.ejercito.doc.web.repo.ProcesoReinicioContDetalleRepository;
 import com.laamware.ejercito.doc.web.repo.ProcesoReinicioContadorRepository;
 import com.laamware.ejercito.doc.web.repo.SecuenciaRadicacionRepository;
 import com.laamware.ejercito.doc.web.serv.RadicadoService;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Component;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -36,33 +39,51 @@ public class RadicacionTask {
 
     @Autowired
     private ProcesoReinicioContadorRepository procesoReinicioContadorRepository;
-    
+
     @Autowired
     private ProcesoReinicioContDetalleRepository procesoReinicioContDetalleRepository;
 
     @Scheduled(cron = "*/30 * * * * *")
     //0 0 0 1 1 ? Cada año el primero de enero a las 12:00 am
     public void reportCurrentTime() {
+        log.info("Inicia el proceso de reinicio de secuencias de radicado...");
+
+        InetAddress addr = null;
+        String hostname = "";
+        String hostAddress = "";
+
+        try {
+            addr = InetAddress.getLocalHost();
+        } catch (UnknownHostException ex) {
+            log.error("Error encontrando la direccion IP de la maquina", ex);
+        }
+        
+        if (addr != null) {
+            hostname = addr.getHostName();
+            hostAddress = addr.getHostAddress();
+            log.info("Iniciando el proceso en la maquina[" + hostname + "], IP[" + hostAddress + "]");
+        }
+
         ProcesoReinicioContador prc = new ProcesoReinicioContador();
         prc.setFechaHoraEjecucion(new Date());
-        //Verificar
-        prc.setIpEjecucion("193");
+        prc.setIpEjecucion(hostname + "-" + hostAddress);
 
         procesoReinicioContadorRepository.save(prc);
 
         List<SecuenciaRadicacion> secuencias = secuenciaRadicacionRepository.findAll();
         for (SecuenciaRadicacion s : secuencias) {
-            
+
             ProcesoReinicioContDetalle prcd = new ProcesoReinicioContDetalle();
             prcd.setSecuencia(s);
             prcd.setSecuenciaNombre(s.getSeqNombre());
             prcd.setProReinicioContador(prc);
             log.info("The time is now {}", dateFormat.format(new Date()) + "-----" + s.getSeqNombre());
             radicadoService.reiniciarsecuencia(s.getSeqNombre(), prcd);
-            
+
             procesoReinicioContDetalleRepository.save(prcd);
         }
 
         log.info("The time is now {}", dateFormat.format(new Date()));
+
     }
 }
