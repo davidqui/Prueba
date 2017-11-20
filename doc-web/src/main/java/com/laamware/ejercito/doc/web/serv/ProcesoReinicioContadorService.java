ad5f95153801dd08ea25dcb344ccd063494e9cae
Edison <edison.gonzalez@controltechcg.com>
2017-11-20 15:58:36 -0500

    issue #138 (SICDI-Controltech) issue-138: Ajuste del log del proceso de reinicio de las secuencias.

--- doc-web/src/main/java/com/laamware/ejercito/doc/web/serv/ProcesoReinicioContadorService.java
+++ doc-web/src/main/java/com/laamware/ejercito/doc/web/serv/ProcesoReinicioContadorService.java
@@ -11,7 +11,6 @@ import java.net.UnknownHostException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import static org.apache.xalan.lib.ExsltDatetime.date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
