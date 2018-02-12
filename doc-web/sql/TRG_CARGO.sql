c29df17487a45c27184cb3e78abb987504f9f59b
Edison <edison.gonzalez@controltechcg.com>
2018-02-12 16:48:36 -0500

    feature-149 (SICDI-Controltech) feature-149:Se modifican los triggers para agregar nuevas columnas para la traza de auditoria.

--- doc-web/sql/TRG_CARGO.sql
+++ doc-web/sql/TRG_CARGO.sql
@@ -3,21 +3,25 @@
 -- Creación del trigger para guardar los cambios que se han registrado en la columna
 -- CAR_NOMBRE de la tabla CARGO.
 
-CREATE OR REPLACE TRIGGER CARGO_TRG AFTER UPDATE ON CARGO
+set SERVEROUTPUT ON
+
+CREATE OR REPLACE TRIGGER CARGO_TRG AFTER INSERT OR UPDATE ON CARGO
 FOR EACH ROW
 BEGIN
-    IF :old.CAR_NOMBRE != :new.CAR_NOMBRE THEN
+    IF nvl(:old.CAR_NOMBRE,'-') != nvl(:new.CAR_NOMBRE,'-') THEN
         begin
             INSERT INTO cargo_historico (
                 car_his_id,
                 car_id,
                 car_nombre_anterior,
+                car_nombre_nuevo,
                 car_his_fec_mod,
                 car_his_usu_mod
             ) VALUES (
                 CARGO_HISTORICO_SEQ.nextval,
                 :new.CAR_ID,
                 :old.CAR_NOMBRE,
+                :new.CAR_NOMBRE,
                 :new.CUANDO_MOD,
                 :new.quien_mod
             );        
--- doc-web/sql/USUARIO_CARGO_TRG.sql
+++ doc-web/sql/USUARIO_CARGO_TRG.sql
@@ -3,46 +3,70 @@
 -- Creación del trigger para guardar los cambios que se han registrado en las columnas
 -- de cargo de la tabla USUARIO.
 
-CREATE OR REPLACE TRIGGER USUARIO_CARGO_TRG AFTER UPDATE ON USUARIO
+set SERVEROUTPUT ON
+
+CREATE OR REPLACE TRIGGER USUARIO_CARGO_TRG AFTER INSERT OR UPDATE ON USUARIO
 FOR EACH ROW
 BEGIN
-    IF (:old.USU_CARGO_PRINCIPAL_ID != :new.USU_CARGO_PRINCIPAL_ID) or  (:old.USU_CARGO1_ID != :new.USU_CARGO1_ID) or
-    (:old.USU_CARGO2_ID != :new.USU_CARGO2_ID) or (:old.USU_CARGO3_ID != :new.USU_CARGO3_ID) or 
-    (:old.USU_CARGO4_ID != :new.USU_CARGO4_ID) or (:old.USU_CARGO5_ID != :new.USU_CARGO5_ID) or
-    (:old.USU_CARGO6_ID != :new.USU_CARGO6_ID) or (:old.USU_CARGO7_ID != :new.USU_CARGO7_ID) or
-    (:old.USU_CARGO8_ID != :new.USU_CARGO8_ID) or (:old.USU_CARGO9_ID != :new.USU_CARGO9_ID) or 
-    (:old.USU_CARGO9_ID != :new.USU_CARGO9_ID) THEN
+    IF (nvl(:old.USU_CARGO_PRINCIPAL_ID,-1) != nvl(:new.USU_CARGO_PRINCIPAL_ID,-1)) or  (nvl(:old.USU_CARGO1_ID,-1) != nvl(:new.USU_CARGO1_ID,-1)) or
+    (nvl(:old.USU_CARGO2_ID,-1) != nvl(:new.USU_CARGO2_ID,-1)) or (nvl(:old.USU_CARGO3_ID,-1) != nvl(:new.USU_CARGO3_ID,-1)) or 
+    (nvl(:old.USU_CARGO4_ID,-1) != nvl(:new.USU_CARGO4_ID,-1)) or (nvl(:old.USU_CARGO5_ID,-1) != nvl(:new.USU_CARGO5_ID,-1)) or
+    (nvl(:old.USU_CARGO6_ID,-1) != nvl(:new.USU_CARGO6_ID,-1)) or (nvl(:old.USU_CARGO7_ID,-1) != nvl(:new.USU_CARGO7_ID,-1)) or
+    (nvl(:old.USU_CARGO8_ID,-1) != nvl(:new.USU_CARGO8_ID,-1)) or (nvl(:old.USU_CARGO9_ID,-1) != nvl(:new.USU_CARGO9_ID,-1)) or 
+    (nvl(:old.USU_CARGO9_ID,-1) != nvl(:new.USU_CARGO9_ID,-1)) THEN
         begin
             INSERT INTO USUARIO_CARGO_HISTORICO (
                 USU_CAR_HIS_ID,
                 USU_ID,
                 CARGO_PRINCIPAL_ANTERIOR,
+                CARGO_PRINCIPAL_NUEVO,
                 CARGO1_ID_ANTERIOR,
+                CARGO1_ID_NUEVO,
                 CARGO2_ID_ANTERIOR,
+                CARGO2_ID_NUEVO,
                 CARGO3_ID_ANTERIOR,
+                CARGO3_ID_NUEVO,
                 CARGO4_ID_ANTERIOR,
+                CARGO4_ID_NUEVO,
                 CARGO5_ID_ANTERIOR,
+                CARGO5_ID_NUEVO,
                 CARGO6_ID_ANTERIOR,
+                CARGO6_ID_NUEVO,
                 CARGO7_ID_ANTERIOR,
+                CARGO7_ID_NUEVO,
                 CARGO8_ID_ANTERIOR,
+                CARGO8_ID_NUEVO,
                 CARGO9_ID_ANTERIOR,
+                CARGO9_ID_NUEVO,
                 CARGO10_ID_ANTERIOR,
+                CARGO10_ID_NUEVO,                
                 CAR_HIS_FEC_MOD,
                 USU_CAR_HIS_USU_MOD
             ) VALUES (
                 USUARIO_CARGO_HISTORICO_SEQ.nextval,
                 :new.usu_id,
                 :old.USU_CARGO_PRINCIPAL_ID,
+                :new.USU_CARGO_PRINCIPAL_ID,
                 :old.USU_CARGO1_ID,
+                :new.USU_CARGO1_ID,
                 :old.USU_CARGO2_ID,
+                :new.USU_CARGO2_ID,
                 :old.USU_CARGO3_ID,
+                :new.USU_CARGO3_ID,
                 :old.USU_CARGO4_ID,
+                :new.USU_CARGO4_ID,
                 :old.USU_CARGO5_ID,
+                :new.USU_CARGO5_ID,
                 :old.USU_CARGO6_ID,
+                :new.USU_CARGO6_ID,
                 :old.USU_CARGO7_ID,
+                :new.USU_CARGO7_ID,
                 :old.USU_CARGO8_ID,
+                :new.USU_CARGO8_ID,
                 :old.USU_CARGO9_ID,
+                :new.USU_CARGO9_ID,
                 :old.USU_CARGO10_ID,
+                :new.USU_CARGO10_ID,
                 :new.CUANDO_MOD,
                 :new.QUIEN_MOD
             );       
