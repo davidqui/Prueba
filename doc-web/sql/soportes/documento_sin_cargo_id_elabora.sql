7c81a31ad2182f4a08500e5ad4af14365530c8d5
egonzalezm <egonzalezm@CAI-C6-55.EJC-CAIMI-NAL.MIL>
2018-05-22 11:36:40 -0500

    jgarcia@controltechcg.com Issue #171 (SICDI-Controltech) hotfix-171: Modificación en FN_PDF_RADIOGRAMA_MAIN para establecer el cargo del usuario que elabora, en caso que este dato no esté presente en DOCUMENTO.

---
+++ doc-web/sql/soportes/documento_sin_cargo_id_elabora.sql
@@ -0,0 +1,36 @@
+-- -----------------------------------------------------------------------------
+-- 2018-05-22 jgarcia@controltechcg.com Issue #171 (SICDI-Controltech)
+-- hotfix-171: Sentencia SQL que construye instrucciones de actualización
+-- sobre la tabla DOCUMENTO cuando no tiene valor en el campo CARGO_ID_ELABORA.
+-- -----------------------------------------------------------------------------
+SELECT
+    'UPDATE DOCUMENTO SET CARGO_ID_ELABORA = '
+     || DOCUMENTO_USU_ELABORA_MAX.CARGO_ID_ELABORA
+     || ' WHERE DOCUMENTO.DOC_ID = '''
+     || DOCUMENTO.DOC_ID
+     || ''';'
+FROM
+    DOCUMENTO
+    JOIN (
+        SELECT
+            DOC_ID,
+            USU_ID_ELABORA,
+            CARGO_ID_ELABORA,
+            MAX(CUANDO)
+        FROM
+            DOCUMENTO_USU_ELABORA
+        WHERE
+            CARGO_ID_ELABORA IS NOT NULL
+        GROUP BY
+            DOC_ID,
+            USU_ID_ELABORA,
+            CARGO_ID_ELABORA
+    ) DOCUMENTO_USU_ELABORA_MAX ON (
+            DOCUMENTO_USU_ELABORA_MAX.DOC_ID = DOCUMENTO.DOC_ID
+        AND
+            DOCUMENTO_USU_ELABORA_MAX.USU_ID_ELABORA = DOCUMENTO.USU_ID_ELABORA
+    )
+WHERE
+        DOCUMENTO.DOC_ASUNTO IS NOT NULL
+    AND
+        DOCUMENTO.CARGO_ID_ELABORA IS NULL;
