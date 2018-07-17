501406ff45b8d2ae42b368a13f9ac571be04e8d3
egonzalezm <egonzalezm@CAI-C6-55.EJC-CAIMI-NAL.MIL>
2018-07-17 16:07:20 -0500

    Issue #175 SICDI-Controltech feature-175 (edison.gonzalez@controltechcg.com): Se cambian las imagenes de los procesos de documentos.

---
+++ doc-web/sql/cambios_contrato_Cenac_334_2017/issue_feature_175.sql
@@ -0,0 +1,10 @@
+--
+-- Author:  edison.gonzalez@controltechcg.com
+-- Created: 17/07/2018 Issue #175 (SICDI-Controltech) feature-175
+--
+
+-- -----------------------------------------------------------------------------
+-- Actualizacion de la imagen del proceso de radicación de documentos
+-- -----------------------------------------------------------------------------
+
+update proceso set pro_imagen = 'documentos-entrada.png' where pro_id = 9;
