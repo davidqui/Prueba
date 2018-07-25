05ec9d191947db5477f2fb62382caf4f0f5d44ec
egonzalezm <egonzalezm@CAI-C6-55.EJC-CAIMI-NAL.MIL>
2018-07-25 18:33:14 -0500

    Issue #182 SICDI-Controltech feature-182 (edison.gonzalez@controltechcg.com): Se adiciona la funcionalidad de administración de envió de correo.

--- doc-web/src/main/resources/static/js/app/header.js
+++ doc-web/src/main/resources/static/js/app/header.js
@@ -5,12 +5,16 @@ function notificar(){
         $("#modal-notificaciones .modal-body").empty();
             var content = '<p>En este panel de configuración puede desactivar las notificaciones del sistema.</p>';
             content = content + '<table class="table">';
+                content = content +'<tr> <th>TIPO DE NOTIFICACIÓN</th><th>ESTADO</th><th>ACCIÓN</th></tr>';
                 $.each(data, function(i, item) {
-                        content = content +'<tr> <td>'+item[1]+'</td>';
+                    content = content +'<tr> <td>'+item[1]+'</td>';
+                    
                     if(item[2] === 1){
-                        content = content +'<td><button id="button_'+item[0]+'" type="button" class="btn btn-success" onclick="cambioEstado('+item[0]+')">Activo</button></td></tr>';
+                        content = content +'<td> <span id="span_'+item[0]+'" class="label label-success">Activo</span></td>';
+                        content = content +'<td><button id="button_'+item[0]+'" type="button" class="btn btn-default" onclick="cambioEstado('+item[0]+')">Desactivar</button></td></tr>';
                     }else{
-                        content = content +'<td><button id="button_'+item[0]+'" type="button" class="btn btn-warning" onclick="cambioEstado('+item[0]+')">Inactivo</button></td></tr>';
+                        content = content +'<td> <span id="span_'+item[0]+'" class="label label-danger">Inactivo</span></td>';
+                        content = content +'<td><button id="button_'+item[0]+'" type="button" class="btn btn-primary" onclick="cambioEstado('+item[0]+')">Activar</button></td></tr>';
                     }
                 });
             content = content +'</table>';        
@@ -24,15 +28,22 @@ function cambioEstado(tnfId){
     type: "POST",
     url: "/notificaciousuario/cambiaEstado/"+ tnfId,
     success: function(data) {
-        var buttonVerificar = $('button_'+tnfId);
         if(data === 0){
-            $('#button_'+tnfId).removeClass("btn btn-success");
-            $('#button_'+tnfId).addClass("btn btn-warning");
-            $("#button_"+tnfId).text("Inactivo");
+            $('#span_'+tnfId).removeClass("label label-success");
+            $('#span_'+tnfId).addClass("label label-danger");
+            $("#span_"+tnfId).html("Inactivo");
+            
+            $('#button_'+tnfId).removeClass("btn btn-default");
+            $('#button_'+tnfId).addClass("btn btn-primary");
+            $("#button_"+tnfId).text("Activar");
         }else{
-            $('#button_'+tnfId).removeClass("btn btn-warning");
-            $('#button_'+tnfId).addClass("btn btn-success");
-            $("#button_"+tnfId).text("Activo");
+            $('#span_'+tnfId).removeClass("label label-danger");
+            $('#span_'+tnfId).addClass("label label-success");
+            $("#span_"+tnfId).html("Activo");
+            
+            $('#button_'+tnfId).removeClass("btn btn-primary");
+            $('#button_'+tnfId).addClass("btn btn-default");
+            $("#button_"+tnfId).text("Desactivar");
         }    
     }
     });
