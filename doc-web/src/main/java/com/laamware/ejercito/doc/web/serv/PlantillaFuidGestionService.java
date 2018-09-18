a4aa684cc3e103c03e1ed4e8f80f7e17bc335940
egonzalezm <egonzalezm@CAI-C6-55.EJC-CAIMI-NAL.MIL>
2018-09-18 17:06:20 -0500

    issue #4 SICDI-Controltech feature-gogs-4 (edison.gonzalez@controltechcg.com): Se realiza el ajuste para verificar el tipo de soporte del documento dependiendo del tipo de proceso al que pertenezca.

--- doc-web/src/main/java/com/laamware/ejercito/doc/web/serv/PlantillaFuidGestionService.java
+++ doc-web/src/main/java/com/laamware/ejercito/doc/web/serv/PlantillaFuidGestionService.java
@@ -10,14 +10,12 @@ import com.aspose.words.Run;
 import com.aspose.words.Table;
 import com.laamware.ejercito.doc.web.dto.KeysValuesAsposeDocxDTO;
 import com.laamware.ejercito.doc.web.entity.AppConstants;
-import com.laamware.ejercito.doc.web.entity.Clasificacion;
 import com.laamware.ejercito.doc.web.entity.Dependencia;
 import com.laamware.ejercito.doc.web.entity.Documento;
 import com.laamware.ejercito.doc.web.entity.DocumentoDependencia;
 import com.laamware.ejercito.doc.web.entity.ExpDocumento;
 import com.laamware.ejercito.doc.web.entity.PlantillaFuidGestion;
-import com.laamware.ejercito.doc.web.entity.PlantillaTransferenciaArchivo;
-import com.laamware.ejercito.doc.web.entity.TransExpedienteDetalle;
+import com.laamware.ejercito.doc.web.entity.Proceso;
 import com.laamware.ejercito.doc.web.entity.TransferenciaArchivo;
 import com.laamware.ejercito.doc.web.entity.TransferenciaArchivoDetalle;
 import com.laamware.ejercito.doc.web.entity.Usuario;
@@ -34,7 +32,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.codec.digest.DigestUtils;
@@ -55,7 +52,7 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @Service
 public class PlantillaFuidGestionService {
-    
+
     private static final Logger LOG = Logger.getLogger(PlantillaFuidGestionService.class.getName());
 
     /**
@@ -66,16 +63,16 @@ public class PlantillaFuidGestionService {
 
     @Autowired
     private DependenciaRepository dependenciaRepository;
-    
+
     @Autowired
     private DocumentoService documentoService;
-    
+
     @Autowired
     private ExpDocumentoService expDocumentoService;
-    
+
     @Autowired
     private TransferenciaArchivoDetalleRepository detalleRepository;
-    
+
     @Autowired
     private TransferenciaArchivoService transferenciaArchivoService;
 
@@ -123,55 +120,56 @@ public class PlantillaFuidGestionService {
 
         return nuevaPlantilla;
     }
-    
-    public void crearDocumentoFuid(TransferenciaArchivo transferenciaArchivo) throws Exception{
-        final PlantillaFuidGestion plantilla = plantillaRepository.findByActivoTrue();
-        final String plantillaPath = ofs.getPath(plantilla.getCodigoOfs());
-        final Document asposeDocument = new Document(plantillaPath);
-        
-        final List<TransferenciaArchivoDetalle> detalles = detalleRepository.findAllByTransferenciaArchivoAndActivo(transferenciaArchivo, 1);
-        if (detalles.size() > 0) {
-            ordenarDetalles(detalles);
-        }
 
-        final Table table = (Table) asposeDocument.getChild(NodeType.TABLE, 2, true);
-        
-        if (table != null) {
-            if (!detalles.isEmpty()) {
-                int indice = 1;
-                for (TransferenciaArchivoDetalle detalle : detalles) {
-                    final String[] cellValues = buildCellValuesDocumento(detalle, indice);
-                    Row row = new Row(asposeDocument);
-                    fillTableRow(asposeDocument, row, cellValues);
-                    table.appendChild(row);
-                    indice++;
+    public void crearDocumentoFuid(TransferenciaArchivo transferenciaArchivo) throws Exception {
+        final List<TransferenciaArchivoDetalle> detalles = detalleRepository.findAllByTransferenciaArchivoAndActivo(transferenciaArchivo, 1);
+
+        if (detalles.size() > 0) {
+            final PlantillaFuidGestion plantilla = plantillaRepository.findByActivoTrue();
+            final String plantillaPath = ofs.getPath(plantilla.getCodigoOfs());
+            final Document asposeDocument = new Document(plantillaPath);
+
+            ordenarDetalles(detalles);
+
+            final Table table = (Table) asposeDocument.getChild(NodeType.TABLE, 2, true);
+
+            if (table != null) {
+                if (!detalles.isEmpty()) {
+                    int indice = 1;
+                    for (TransferenciaArchivoDetalle detalle : detalles) {
+                        final String[] cellValues = buildCellValuesDocumento(detalle, indice);
+                        Row row = new Row(asposeDocument);
+                        fillTableRow(asposeDocument, row, cellValues);
+                        table.appendChild(row);
+                        indice++;
+                    }
                 }
             }
-        }
-                
-        final License asposeLicense = new License();
-        final Resource resource = new ClassPathResource("Aspose.Words.lic");
-        asposeLicense.setLicense(resource.getInputStream());
-        final KeysValuesAsposeDocxDTO asposeMap = crearMapaAspose(transferenciaArchivo);
-        asposeDocument.getMailMerge().execute(asposeMap.getNombres(), asposeMap.getValues());
-        
-        final File tmpFile = File.createTempFile("_sigdi_temp_", ".pdf");
-        asposeDocument.save(tmpFile.getPath());
-        
-        final OFSEntry ofsEntry = ofs.readPDFAspose(tmpFile);
-        final String codigoActaOFS = ofs.save(ofsEntry.getContent(), AppConstants.MIME_TYPE_PDF);
-        
-        transferenciaArchivo.setFuid(codigoActaOFS);
-        transferenciaArchivoService.actualizarTransferenciaArchivo(transferenciaArchivo);
-        
-        try {
-            tmpFile.delete();
-        } catch (Exception ex) {
-            LOG.log(Level.SEVERE, null, ex);
+
+            final License asposeLicense = new License();
+            final Resource resource = new ClassPathResource("Aspose.Words.lic");
+            asposeLicense.setLicense(resource.getInputStream());
+            final KeysValuesAsposeDocxDTO asposeMap = crearMapaAspose(transferenciaArchivo);
+            asposeDocument.getMailMerge().execute(asposeMap.getNombres(), asposeMap.getValues());
+
+            final File tmpFile = File.createTempFile("_sigdi_temp_", ".pdf");
+            asposeDocument.save(tmpFile.getPath());
+
+            final OFSEntry ofsEntry = ofs.readPDFAspose(tmpFile);
+            final String codigoActaOFS = ofs.save(ofsEntry.getContent(), AppConstants.MIME_TYPE_PDF);
+
+            transferenciaArchivo.setFuid(codigoActaOFS);
+            transferenciaArchivoService.actualizarTransferenciaArchivo(transferenciaArchivo);
+
             try {
-                tmpFile.deleteOnExit();
-            } catch (Exception ex1) {
-                LOG.log(Level.SEVERE, null, ex1);
+                tmpFile.delete();
+            } catch (Exception ex) {
+                LOG.log(Level.SEVERE, null, ex);
+                try {
+                    tmpFile.deleteOnExit();
+                } catch (Exception ex1) {
+                    LOG.log(Level.SEVERE, null, ex1);
+                }
             }
         }
     }
@@ -190,14 +188,14 @@ public class PlantillaFuidGestionService {
         Integer unidadIdPadre = dependenciaRepository.findUnidadID(transferenciaArchivo.getOrigenDependencia().getId());
         Dependencia unidadPadre = dependenciaRepository.findOne(unidadIdPadre);
         map.put("NOMBRE_UNIDAD_PADRE", unidadPadre.getNombre());
-        
-        if(transferenciaArchivo.getOrigenDependencia().getPadre() != null){
+
+        if (transferenciaArchivo.getOrigenDependencia().getPadre() != null) {
             Dependencia dependenciaPadre = dependenciaRepository.findOne(transferenciaArchivo.getOrigenDependencia().getPadre());
             map.put("NOMBRE_DEPENDENCIA_PADRE_ORIGEN", dependenciaPadre.getNombre());
-        }else{
+        } else {
             map.put("NOMBRE_DEPENDENCIA_PADRE_ORIGEN", transferenciaArchivo.getOrigenDependencia().getNombre());
         }
-        
+
         map.put("NOMBRE_DEPENDENCIA_ORIGEN", transferenciaArchivo.getOrigenDependencia().getNombre());
 
         map.put("JUSTIFICACION", transferenciaArchivo.getJustificacion());
@@ -208,31 +206,31 @@ public class PlantillaFuidGestionService {
         int year = cal.get(Calendar.YEAR);
         int month = cal.get(Calendar.MONTH);
         int day = cal.get(Calendar.DAY_OF_MONTH);
-        
+
         map.put("ANIO_TRAN", year);
         map.put("MES_TRAN", month);
         map.put("DIA_TRAN", day);
-        
-        if(transferenciaArchivo.getDocId() != null){
+
+        if (transferenciaArchivo.getDocId() != null) {
             map.put("NUM_TRAN", transferenciaArchivo.getDocId().getRadicado());
         }
-        
+
         Integer numFoliosTotal = transferenciaArchivoService.obetenerNumeroFoliosPorTransferenciaArchivo(transferenciaArchivo);
         map.put("NUM_FOLIOS_TOTAL", numFoliosTotal);
-        
-        final String usuarioEmisor = transferenciaArchivo.getOrigenGrado().getId()+". "+transferenciaArchivo.getOrigenUsuario().getNombre();
+
+        final String usuarioEmisor = transferenciaArchivo.getOrigenGrado().getId() + ". " + transferenciaArchivo.getOrigenUsuario().getNombre();
         map.put("NOMBRE_USUARIO_EMISOR", usuarioEmisor);
         map.put("CARGO_USUARIO_EMISOR", transferenciaArchivo.getUsuOrigenCargo().getCarNombre());
         map.put("DEPENDENCIA_ORIGEN_CIUDAD", transferenciaArchivo.getOrigenDependencia().getCiudad());
-        
-        final String usuarioDestinatario = transferenciaArchivo.getDestinoGrado().getId()+". "+transferenciaArchivo.getDestinoUsuario().getNombre();
+
+        final String usuarioDestinatario = transferenciaArchivo.getDestinoGrado().getId() + ". " + transferenciaArchivo.getDestinoUsuario().getNombre();
         map.put("NOMBRE_USUARIO_RECEPTOR", usuarioDestinatario);
         map.put("CARGO_USUARIO_RECEPTOR", transferenciaArchivo.getUsuDestinoCargo().getCarNombre());
         map.put("DEPENDENCIA_DESTINO_CIUDAD", transferenciaArchivo.getDestinoDependencia().getCiudad());
-        
+
         final String fechaTransferencia = documentDateFormatter.format(transferenciaArchivo.getFechaCreacion());
         map.put("FECHA_TRANSFERENCIA", fechaTransferencia);
-        
+
         final File creadorFirma = getImagenFirma(transferenciaArchivo.getOrigenUsuario());
         if (creadorFirma != null) {
             try {
@@ -241,8 +239,6 @@ public class PlantillaFuidGestionService {
                 LOG.log(Level.SEVERE, "img_firma_emisor", ex);
             }
         }
-        
-        
 
         final File destinoFirma = getImagenFirma(transferenciaArchivo.getDestinoUsuario());
         if (destinoFirma != null) {
@@ -254,10 +250,9 @@ public class PlantillaFuidGestionService {
         }
         return map;
     }
-    
+
     /**
-     * Construye los valores para las celdas de una fila de la tabla de
-     * FUID.
+     * Construye los valores para las celdas de una fila de la tabla de FUID.
      *
      * @param detalle Registro de detalle de transferencia.
      * @return Arreglo de valores del detalle de transferencia.
@@ -266,34 +261,40 @@ public class PlantillaFuidGestionService {
         final SimpleDateFormat documentDateFormatter = new SimpleDateFormat(Global.DATE_FORMAT, Global.COLOMBIA);
         final DocumentoDependencia documentoDependencia = detalle.getDocumentoDependencia();
         final Documento documento = documentoDependencia.getDocumento();
-        
+
         final String numOrden = String.valueOf(indice);
         final String codigoDependendia = documento.getElabora().getDependencia().getCodigo();
         final String codigoTrd = documento.getTrd().getCodigo();
-        final String codigo = codigoDependendia+" - "+codigoTrd;
+        final String codigo = codigoDependendia + " - " + codigoTrd;
         final String asunto = documento.getAsunto();
-        
+
         String fecRadicado = documentDateFormatter.format(documento.getDocFecRadicado());
         final String numCaja = "---------------";
         final String numCarpeta = "---------------";
         final String tomo = "---------------";
         final String otro = "NAS-IMI";
-        
+
         final int numFolios = documentoService.obtenerNumeroFolios(documento);
         final String numeroFolios = String.valueOf(numFolios);
+
+        final String soporte;
+        if(documento.getInstancia().getProceso().getId().equals(Proceso.ID_TIPO_PROCESO_REGISTRAR_Y_CONSULTAR_DOCUMENTOS)){
+            soporte = "DIGITALIZADO";
+        }else{
+            soporte = "ELECTRONICO";
+        }
         
-        final String soporte = "ELECTRONICO";
         final String freConsulta = "BAJA";
         final String notas;
         ExpDocumento expDocumento = expDocumentoService.findByDocumento(documento);
-        if(expDocumento != null){
+        if (expDocumento != null) {
             notas = expDocumento.getExpId().getExpNombre();
-        }else{
+        } else {
             notas = "EXPEDIENTE ELECTRONICO";
         }
-        return new String[]{numOrden, codigo, asunto, fecRadicado,fecRadicado, numCaja,numCarpeta,tomo,otro,numeroFolios,soporte,freConsulta,notas};
+        return new String[]{numOrden, codigo, asunto, fecRadicado, fecRadicado, numCaja, numCarpeta, tomo, otro, numeroFolios, soporte, freConsulta, notas};
     }
-    
+
     /**
      * Ordena la lista de detalles según el código de la TRD asociada.
      *
@@ -311,7 +312,7 @@ public class PlantillaFuidGestionService {
             }
         });
     }
-    
+
     /**
      * Llena la fila de table.
      *
@@ -331,9 +332,10 @@ public class PlantillaFuidGestionService {
             row.appendChild(cell);
         }
     }
-    
+
     /**
      * Retorna el archivo de la firma del usuario
+     *
      * @param usuario
      * @return Archivo de firma
      */
