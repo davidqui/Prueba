e4a781da62d7104907a128d55b34601402998297
egonzalezm <egonzalezm@CAI-C6-55.EJC-CAIMI-NAL.MIL>
2018-09-26 10:31:03 -0500

    issue #10 SICDI-Controltech feature-gogs-10 (edison.gonzalez@controltechcg.com): Se realiza el traslado de metodo.

--- doc-web/src/main/java/com/laamware/ejercito/doc/web/repo/DependenciaRepository.java
+++ doc-web/src/main/java/com/laamware/ejercito/doc/web/repo/DependenciaRepository.java
@@ -66,26 +66,44 @@ public interface DependenciaRepository extends GenJpaRepository<Dependencia, Int
     * 2018-02-14 edison.gonzalez@controltechcg.com issue #150
     * (SICDI-Controltech) hotfix-150: Ajuste para desplegar visualmente  las 
     * unidades padres en el arbol, cuando se reasigna un documento.
-    */
+     */
     List<Dependencia> findByActivoAndPadreAndDepIndEnvioDocumentos(boolean activo, Integer padreId, boolean indUnidadPadre, Sort sort);
 
     /*
     * 2018-08-17 samuel.delgado@controltechcg.com issue #7 gogs
     * (SICDI-Controltech) feature-gogs-7: Lista las dependencias que tengan un 
     *  usuario registro dado por un usuario.
-    */
+     */
     public List<Dependencia> findActivoByUsuarioRegistro(Usuario usuario);
-    
+
     @Query(value = "select t from Dependencia t where t.activo = 1 and (LOWER(t.nombre) like %:pNombre% or"
             + " LOWER(t.sigla) like %:pNombre% or LOWER(t.depCodigoLdap) like %:pNombre% or"
             + " LOWER(t.depCodigoLdap) like %:pNombre%)"
             + " order by t.nombre")
     public Page<Dependencia> findDependenciaAdminActivo(@Param("pNombre") String pNombre, Pageable pageable);
-    
+
     @Query(value = "select t from Dependencia t where (LOWER(t.nombre) like %:pNombre% or"
-        + " LOWER(t.sigla) like %:pNombre% or LOWER(t.depCodigoLdap) like %:pNombre% or"
-        + " LOWER(t.depCodigoLdap) like %:pNombre%)"
-        + " order by t.nombre")
+            + " LOWER(t.sigla) like %:pNombre% or LOWER(t.depCodigoLdap) like %:pNombre% or"
+            + " LOWER(t.depCodigoLdap) like %:pNombre%)"
+            + " order by t.nombre")
     public Page<Dependencia> findDependenciaAdmin(@Param("pNombre") String pNombre, Pageable pageable);
-    
-}   
+
+    @Query(value = ""
+            + "SELECT DEP_SIGLA\n"
+            + "FROM DEPENDENCIA WHERE DEP_SIGLA IS NOT NULL\n"
+            + "START WITH DEP_ID = :depId\n"
+            + "CONNECT BY DEP_ID = PRIOR DEP_PADRE\n"
+            + "ORDER BY LEVEL DESC, DEP_SIGLA", nativeQuery = true)
+    List<Object[]> retornaSiglasDependientes(@Param("depId") Integer depId);
+
+    @Query(value = ""
+            + "select dep_sigla\n"
+            + "from(\n"
+            + " SELECT DEP_SIGLA, rownum indice\n"
+            + " FROM DEPENDENCIA WHERE DEP_SIGLA IS NOT NULL\n"
+            + " START WITH DEP_ID = :depId\n"
+            + " CONNECT BY DEP_ID = PRIOR DEP_PADRE\n"
+            + " ORDER BY LEVEL asc, DEP_SIGLA)\n"
+            + "where indice = 1", nativeQuery = true)
+    String retornaSiglaSuperior(@Param("depId") Integer depId);
+}
